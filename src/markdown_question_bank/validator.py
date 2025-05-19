526e7a711ea2b83cb75423d61dfe130eeff112da
Hugo López-Fernández <hlfernandez@uvigo.es>
2025-05-19 20:25:52 +0200

    Improves error messages

--- src/markdown_question_bank/validator.py
+++ src/markdown_question_bank/validator.py
@@ -10,7 +10,7 @@ class QuestionValidator:
     def validate(self, question: Question) -> None:
         for lang in question.get_languages():
             if not any(answer.get_translation(lang) for answer in question.get_right_answers()):
-                raise ValueError(f"A pregunta non ten respostas correctas en {lang}.")
+                raise ValueError(f"A pregunta non ten respostas correctas en {lang}. Enunciado: {question.get_statement().get_translation(lang)}")
             if len([answer for answer in question.get_wrong_answers() if answer.get_translation(lang)]) < self.min_wrong:
                 raise ValueError(
                     f"A pregunta ten menos de {self.min_wrong} respostas incorrectas en {lang}. Enunciado: {question.get_statement().get_translation(lang)}"
