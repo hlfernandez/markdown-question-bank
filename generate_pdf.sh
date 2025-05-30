86a86f8175b9bf6c8c3b1ccc9d9081abef9fbd83
lipido <lipido@gmail.com>
2025-05-30 11:49:08 +0200

    Enhances PDF generation script to include option for keeping intermediate HTML files
    Adds an extended usage explanation

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -2,16 +2,26 @@
 
 # Usage function
 usage() {
-    echo "Usage: $0 -d <markdown_models_dir> -h <heading_file> -c <css_file>"
+    echo "Usage: $0 -d <markdown_models_dir> -h <heading_file> -c <css_file> [-k]"
+    echo ""
+    echo "Options:"
+    echo "  -d <markdown_models_dir>  Directory containing the Markdown files to process."
+    echo "  -h <heading_file>         File to be prepended to each Markdown file. If it's a .jinja2 file, it will be rendered with 'filename' variable."
+    echo "  -c <css_file>             CSS file to style the PDF output."
+    echo "  -k                        Keep the intermediate HTML file."
     exit 1
 }
 
+# Initialize KEEP_HTML flag
+KEEP_HTML=false
+
 # Parse command-line arguments
-while getopts "d:h:c:" opt; do
+while getopts "d:h:c:k" opt; do
     case $opt in
         d) MARDOWN_MODELS_DIR="$OPTARG" ;;
         h) HEADING="$OPTARG" ;;
         c) CSS="$OPTARG" ;;
+        k) KEEP_HTML=true ;;
         *) usage ;;
     esac
 done
@@ -32,10 +42,16 @@ for file in $(find "$MARDOWN_MODELS_DIR" -type f -name "*.md"); do
         # Render the Jinja2 template
         RENDERED_HEADING=$(mktemp /tmp/rendered_heading.XXXXXX)
         jinja2 "$HEADING" -D filename="$FILE" > "$RENDERED_HEADING"
-        pandoc "$RENDERED_HEADING" "$file" --quiet --css="$CSS" --pdf-engine=weasyprint -o "$PDF_FILE"
+        if [ "$KEEP_HTML" = true ]; then
+	        pandoc "$RENDERED_HEADING" "$file" --quiet --css="$CSS" -o "${PDF_FILE}.html"
+        fi
+	    pandoc "$RENDERED_HEADING" "$file" --quiet --css="$CSS" --pdf-engine=weasyprint -o "$PDF_FILE"
         rm "$RENDERED_HEADING"
     else
         # Use the HEADING file directly
+        if [ "$KEEP_HTML" = true ]; then
+            pandoc "$HEADING" "$file" --quiet --css="$CSS" -o "${PDF_FILE}.html"
+        fi
         pandoc "$HEADING" "$file" --quiet --css="$CSS" --pdf-engine=weasyprint -o "$PDF_FILE"
     fi
 done
